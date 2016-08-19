c5c82c92dbdc0aa67b01d18f325d7c102da6f48a
Yoo Hyun Sik <dbgustlr92@naver.com>
2016-08-20 01:09:28 +0900

    서버 클라이언트 테스트코드

---
+++ socketTest/src/socketTest.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Created by macgongmon on 8/18/16.
+ */
+public class socketTest {
+
+    public static void main(String[] args){
+
+        try {
+
+
+            System.out.println("Socket 연결");
+            Socket socket = new Socket("127.0.0.1",50007);
+            BufferedReader networkReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedWriter networkWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            try{
+                String pushItem = "get_market_list_bob";
+                networkWriter.write(pushItem);
+                networkWriter.flush();
+
+
+                //String receiveItem = networkReader.readLine();
+                char[] temp = new char[1024];
+                networkReader.read(temp);
+
+                System.out.println("received item was : "+new String(temp).trim());
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
